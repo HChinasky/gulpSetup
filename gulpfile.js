325d610e36c10ece9dfd246c52ea45b474b9023b
Yaroslav <yarik_watson@hotmail.com>
2019-08-21 20:27:35 +0300

    gulp config. Compile scss to css. Setup Browser sync

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const autoPrefixer = require('gulp-autoprefixer');
+
+// compile scss to css
+function style() {
+  return gulp.src('./scss/**/*.scss')
+  .pipe(plumber({
+    errorHandler: notify.onError(function(err) {
+      return {
+        title: 'Styles',
+        message: err.message
+      }
+    })
+  }))
+  .pipe(sass())
+  .pipe(autoPrefixer({
+    browsers: ['last 6 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
